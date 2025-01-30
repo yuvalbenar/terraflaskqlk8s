7973a76b618e42c5ec3c78f2a74564e55facfba0
yuvalbenar <yuvalbenar@gmail.com>
2025-01-30 21:18:01 +0200

    Use Environment Variables (Recommended)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,9 @@
-provider "google" { 
+provider "google" {
   project     = "terraflaskqlk8s"  # Replace with your GCP project ID
   region      = "us-east1"          # Use the region (not zone, e.g., "us-east1")
-  credentials = jsondecode(var.GCP_CREDENTIALS)  # Use jsondecode to convert the JSON string into a usable map
+  credentials = jsondecode(var.GCP_CREDENTIALS)  # Decode the JSON string into a usable map
 }
+
 
 resource "google_container_cluster" "flasksql_cluster" {
   name               = "flasksql-cluster"
