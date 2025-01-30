a1c31dd64f5c20039292b5c9ff28353d8b39c599
yuvalbenar <yuvalbenar@gmail.com>
2025-01-30 22:55:27 +0200

    removing the variable.tf and my estonia

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" { 
   project     = "terraflaskqlk8s"  # Replace with your GCP project ID
   region      = "us-east1"          # Use the region (not zone, e.g., "us-east1")
-  credentials = file(var.GOOGLE_APPLICATION_CREDENTIALS)  # Use the credentials from the environment variable
+  credentials = file(var.GCP_CREDENTIALS)  # Use the credentials from the GCP_CREDENTIALS environment variable
 }
 
 resource "google_container_cluster" "flasksql_cluster" {
