b44288b4892182058541c3eaada4da3a092c2c97
yuvalbenar <yuvalbenar@gmail.com>
2025-01-30 22:52:04 +0200

    removing the variable.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-provider "google" {
-  project = "terraflaskqlk8s"  # Replace with your GCP project ID
-  region  = "us-east1"          # Use the region (not zone, e.g., "us-east1")
+provider "google" { 
+  project     = "terraflaskqlk8s"  # Replace with your GCP project ID
+  region      = "us-east1"          # Use the region (not zone, e.g., "us-east1")
+  credentials = file(var.GOOGLE_APPLICATION_CREDENTIALS)  # Use the credentials from the environment variable
 }
 
 resource "google_container_cluster" "flasksql_cluster" {
--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "GCP_CREDENTIALS" {
-  description = "Path to the GCP service account credentials JSON file"
-  type        = string
-}
