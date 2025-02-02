fe6e42316583bc80bcf95836cbc184a4f012b5e4
yuvalbenar <yuvalbenar@gmail.com>
2025-02-02 10:56:19 +0200

    cahnged main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,19 @@
 provider "google" {
   project     = "terraflaskqlk8s"
   region      = "us-east1"
-  credentials = file(var.credentials_path)
+  credentials = file(var.credentials_path)  # Path to the credentials
 }
-
 
 resource "google_container_cluster" "flasksql_cluster" {
   name               = "flasksql-cluster"
-  location           = "us-east1-b"  # Zone (e.g., "us-east1-b")
-  initial_node_count = 2
+  location           = "us-east1-b"   # Update this if needed
+  initial_node_count = 1               # You can change the node count as needed
 
+  # Ensure the right service account is set
   node_config {
-    machine_type = "e2-medium"
+    service_account = "terracicd-sa@terraflaskqlk8s.iam.gserviceaccount.com"
   }
+
+  # Optional settings like enabling Kubernetes Dashboard, etc.
+  enable_kubernetes_alpha = true
 }
