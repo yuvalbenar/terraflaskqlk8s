241ee4124068aed5d88b9b3725b96f0ed032ce42
yuvalbenar <yuvalbenar@gmail.com>
2025-02-14 23:40:46 +0200

    added way too many stuff in one go, building docker image and pushing it as well as changing app.py initdbsql adding a servicemonitor yaml and so on, hopefully not too many probloms

--- init-db.sql
+++ init-db.sql
@@ -1,13 +1,23 @@
--- init-db.sql
+-- Create the database if it doesn't exist and switch to it
 CREATE DATABASE IF NOT EXISTS flaskdb;
 USE flaskdb;
 
+-- Create the visitor_counter table
+CREATE TABLE IF NOT EXISTS visitor_counter (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    count INT NOT NULL DEFAULT 0
+);
+
+-- Initialize the counter with a starting value of 0
+INSERT INTO visitor_counter (count) VALUES (0);
+
+-- Create the images table
 CREATE TABLE IF NOT EXISTS images (
     id INT AUTO_INCREMENT PRIMARY KEY,
     url VARCHAR(255) UNIQUE NOT NULL
 );
 
--- Insert GIF URLs (without checking for duplicates)
+-- Insert GIF URLs (duplicates will be ignored)
 INSERT IGNORE INTO images (url) 
 VALUES 
     ('https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXpyZDlzODRsejNicHc4dDVvNXRscjdybDNldXc1eWhqejM3cjY4ciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QvBoMEcQ7DQXK/giphy.gif'),
